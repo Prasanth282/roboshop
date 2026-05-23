4297e12d5f64e64fb7c7a903653e585458ec15be
prasanth <durgaprasanth003@gmail.com>
2026-05-23 14:51:17 +0530

    hello world

--- ec2-r53-records.sh
+++ ec2-r53-records.sh
@@ -12,9 +12,33 @@ INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-
     if [ $instance -ne "frontend" ]
     then 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        $RECORD_NAME="$instance.DOMAIN_NAME"
     else 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        $RECORD_NAME="$instance.DOMAIN_NAME"
     fi 
     echo "public IP of $instance is :: $IP"
-done 
-     
+
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+
+{
+  "Comment": "create and Update record to reflect new IP address",
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "$RECORD_NAME",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [
+          {
+            "Value": "$IP"
+          }
+        ]
+      }
+    }
+  ]
+}  '
+done
