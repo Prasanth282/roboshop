5acbfeb4cb7379fb535bc52a0af706b3be4f6f9b
prasanth <durgaprasanth003@gmail.com>
2026-05-23 15:10:26 +0530

    hello world

--- ec2-r53-records.sh
+++ ec2-r53-records.sh
@@ -29,12 +29,12 @@ aws route53 change-resource-record-sets \
     {
       "Action": "UPSERT",
       "ResourceRecordSet": {
-        "Name": "$RECORD_NAME",
+        "Name": "'$RECORD_NAME'",
         "Type": "A",
         "TTL": 1,
         "ResourceRecords": [
           {
-            "Value": "$IP"
+            "Value": "'$IP'"
           }
         ]
       }
