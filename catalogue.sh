057b30b615ba6430ae54cc671fd2f598a9d30409
prasanth <durgaprasanth003@gmail.com>
2026-05-25 13:12:44 +0530

    hello world

--- catalogue.sh
+++ catalogue.sh
@@ -93,10 +93,10 @@ dnf install mongodb-mongosh -y | &>> $LOG_FILE
 VALIDATE $? "installing mongodb shell"
 
 
-STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+STATUS=$(mongosh --host mongodb.prasanth.cloud --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.prasanth.cloud </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
